157a4532fe1056eda75b62f5658effed641ef7dd
Macaza Tshabalala <macazatshabalala726@gmail.com>
2024-12-01 19:46:39 +0200

    Proper project folder

---
+++ SimpleBankingSystem/build/classes/simplebankingsystem/FastCash.rs
@@ -0,0 +1,8 @@
+FastCash$3
+FastCash$4
+FastCash
+FastCash$1
+FastCash$2
+FastCash$7
+FastCash$5
+FastCash$6
---
+++ SimpleBankingSystem/src/Transaction.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author SuperiorAuto
+ */
+public class Transaction {
+
+    public Transaction(String cardNo) {
+    }
+    
+}
